0cf6a5335181b89dfcf7dd4e372572c9e0aabb09
a455jldvmsrwll1a <51287554+a455jldvmsrwll1a@users.noreply.github.com>
2025-02-05 16:04:25 -0600

    Add payload classes and register them.

---
+++ src/main/java/balls/jl/mcofflineauth/net/PubkeyQueryPayload.java
@@ -0,0 +1,23 @@
+package balls.jl.mcofflineauth.net;
+
+import balls.jl.mcofflineauth.Constants;
+import net.minecraft.network.RegistryByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.packet.CustomPayload;
+
+/**
+ * Tells the client to send over its public key.
+ * */
+public class PubkeyQueryPayload implements CustomPayload  {
+    public static final CustomPayload.Id<PubkeyQueryPayload> ID = new CustomPayload.Id<>(Constants.PUBKEY_QUERY_PACKET_ID);
+    public static final PacketCodec<RegistryByteBuf, PubkeyQueryPayload> CODEC = PacketCodec.of(PubkeyQueryPayload::write, PubkeyQueryPayload::new);
+
+    public PubkeyQueryPayload() {}
+    public PubkeyQueryPayload(RegistryByteBuf buf) {}
+    public void write(RegistryByteBuf buf) {}
+
+    @Override
+    public CustomPayload.Id<? extends CustomPayload> getId() {
+        return ID;
+    }
+}
