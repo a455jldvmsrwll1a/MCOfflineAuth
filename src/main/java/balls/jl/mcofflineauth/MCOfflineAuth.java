2627f66f79eee84e8338c1c0534a380c3728c2c9
Jann Atienza <minelolpjl@gmail.com>
2025-02-05 14:45:48 -0600

    Initial commit.

---
+++ src/main/java/balls/jl/mcofflineauth/MCOfflineAuth.java
@@ -0,0 +1,10 @@
+package balls.jl.mcofflineauth;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MCOfflineAuth implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
