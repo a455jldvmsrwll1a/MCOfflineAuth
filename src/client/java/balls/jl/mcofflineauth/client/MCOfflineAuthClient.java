ef47d48525a02e7538853f313964e243625f84bf
a455jldvmsrwll1a <51287554+a455jldvmsrwll1a@users.noreply.github.com>
2025-02-05 14:45:48 -0600

    Initial commit.

---
+++ src/client/java/balls/jl/mcofflineauth/client/MCOfflineAuthClient.java
@@ -0,0 +1,10 @@
+package balls.jl.mcofflineauth.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCOfflineAuthClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
