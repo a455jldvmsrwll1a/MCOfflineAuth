8bb9427c1bcc9b2cb6cab51248ff22fa8d263159
a455jldvmsrwll1a <51287554+a455jldvmsrwll1a@users.noreply.github.com>
2025-02-09 19:23:26 -0600

    Use bad packets API for cross-loader mod communication.
    https://github.com/badasintended/badpackets

--- src/main/java/balls/jl/mcofflineauth/MCOfflineAuth.java
+++ src/main/java/balls/jl/mcofflineauth/MCOfflineAuth.java
@@ -3,6 +3,13 @@ package balls.jl.mcofflineauth;
 import balls.jl.mcofflineauth.command.Commands;
 import balls.jl.mcofflineauth.net.*;
 import balls.jl.mcofflineauth.util.KeyEncode;
+import lol.bai.badpackets.api.PacketReadyCallback;
+import lol.bai.badpackets.api.PacketReceiver;
+import lol.bai.badpackets.api.config.ConfigPackets;
+import lol.bai.badpackets.api.config.ConfigTaskExecutor;
+import lol.bai.badpackets.api.config.ServerConfigContext;
+import lol.bai.badpackets.api.play.PlayPackets;
+import lol.bai.badpackets.api.play.ServerPlayContext;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.networking.v1.*;
@@ -25,22 +32,6 @@ import java.util.Objects;
 import java.util.UUID;
 
 public class MCOfflineAuth implements ModInitializer {
-    static class ChallengeState {
-        static final int CHALLENGE_TIMEOUT = 5;
-
-        private final Instant expiration;
-        public final byte[] data;
-
-        public ChallengeState(byte[] data) {
-            expiration = Instant.now().plusSeconds(CHALLENGE_TIMEOUT);
-            this.data = data;
-        }
-
-        public boolean isExpired() {
-            return Instant.now().isAfter(expiration);
-        }
-    }
-
     private static final Logger LOGGER = LoggerFactory.getLogger(Constants.MOD_ID);
 
     private static final HashMap<UUID, ChallengeState> CHALLENGES = new HashMap<>();
@@ -58,68 +49,21 @@ public class MCOfflineAuth implements ModInitializer {
     }
 
     private static void registerPacketPayloads() {
-        PayloadTypeRegistry.configurationS2C().register(LoginChallengePayload.ID, LoginChallengePayload.CODEC);
-        PayloadTypeRegistry.configurationC2S().register(LoginResponsePayload.ID, LoginResponsePayload.CODEC);
+        ConfigPackets.registerClientChannel(LoginChallengePayload.ID, LoginChallengePayload.CODEC);
+        ConfigPackets.registerServerChannel(LoginResponsePayload.ID, LoginResponsePayload.CODEC);
 
-        PayloadTypeRegistry.playS2C().register(PubkeyQueryPayload.ID, PubkeyQueryPayload.CODEC);
-        PayloadTypeRegistry.playC2S().register(PubkeyBindPayload.ID, PubkeyBindPayload.CODEC);
+        PlayPackets.registerClientChannel(PubkeyQueryPayload.ID, PubkeyQueryPayload.CODEC);
+        PlayPackets.registerServerChannel(PubkeyBindPayload.ID, PubkeyBindPayload.CODEC);
     }
 
     private static void registerEventCallbacks() {
-        ServerConfigurationConnectionEvents.BEFORE_CONFIGURE.register(MCOfflineAuth::onPreConfigure);
-        ServerConfigurationNetworking.registerGlobalReceiver(LoginResponsePayload.ID, MCOfflineAuth::onReceivedChallengeResponse);
+        ConfigPackets.registerTask(Constants.LOGIN_TASK, new LoginTask());
+        ConfigPackets.registerServerReceiver(LoginResponsePayload.ID, new LoginResponseReceiver());
+        PlayPackets.registerServerReceiver(PubkeyBindPayload.ID, new PubkeyBindReceiver());
+
         ServerPlayConnectionEvents.JOIN.register(MCOfflineAuth::onPlayerJoin);
-        ServerPlayNetworking.registerGlobalReceiver(PubkeyBindPayload.ID, MCOfflineAuth::onReceivedClientBind);
 
         CommandRegistrationCallback.EVENT.register(Commands::register);
-    }
-
-    private static void onPreConfigure(ServerConfigurationNetworkHandler handler, MinecraftServer server) {
-        server.execute(MCOfflineAuth::checkForExpiredChallenges);
-        if (!ServerConfig.isEnforcing() || server.isSingleplayer()) {
-            LOGGER.warn("MCOfflineAuth is on standby; won't do anything here.");
-            return;
-        }
-
-        if (!ServerConfigurationNetworking.canSend(handler, LoginChallengePayload.ID)) {
-            handler.onDisconnected(new DisconnectionInfo(Text.of("Client does not have MCOfflineAuth installed.")));
-            handler.disconnect(Text.of("Access denied. D:"));
-            return;
-        }
-
-        handler.addTask(new LoginTask());
-        LoginChallengePayload payload = spawnChallenge();
-        ServerConfigurationNetworking.send(handler, payload);
-    }
-
-    private static void onReceivedChallengeResponse(LoginResponsePayload payload, ServerConfigurationNetworking.Context context) {
-        checkForExpiredChallenges();
-        context.networkHandler().completeTask(new LoginTask().getKey());
-
-        ChallengeState state = CHALLENGES.remove(payload.id);
-        if (state == null) {
-            context.networkHandler().disconnect(Text.of("Signature verification failed due to timeout. Please try again."));
-            return;
-        }
-
-        if (!AuthorisedKeys.KEYS.containsKey(payload.user)) {
-            LOGGER.warn("Connecting user {} is not in the database.", payload.user);
-
-            // Skip verification for unbound usernames if it's allowed.
-            if (ServerConfig.allowsUnboundUsers())
-                return;
-
-            // Else, kick them.
-            context.networkHandler().disconnect(Text.of("You cannot join without being bound in advance. Contact a server admin to let you in."));
-            return;
-        }
-
-        if (!AuthorisedKeys.verifySignature(payload.user, state.data, payload.signature)) {
-            context.networkHandler().disconnect(Text.of("Unable to verify your identity (incorrect signature); do you have the correct key?"));
-            return;
-        }
-
-        LOGGER.info("Verified {}'s identity successfully.", payload.user);
     }
 
     private static void onPlayerJoin(ServerPlayNetworkHandler handler, PacketSender sender, MinecraftServer server) {
@@ -130,23 +74,6 @@ public class MCOfflineAuth implements ModInitializer {
             handler.player.sendMessage(Text.literal("No key is bound to this username; anyone can join with this name."));
             handler.player.sendMessage(Text.literal("§aClick this text or type \"§f/offauth bind§a\" to bind your key!§r").setStyle(Style.EMPTY.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("Click --> /offauth bind"))).withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/offauth bind"))));
         }
-    }
-
-    private static void onReceivedClientBind(PubkeyBindPayload payload, ServerPlayNetworking.Context context) {
-        context.server().execute(() -> {
-            checkForExpiredChallenges();
-
-            if (!Objects.equals(payload.user, context.player().getName().getString())) {
-                LOGGER.warn("Public-key payload username \"{}\" does not match the user who sent it!", payload.user);
-                context.player().sendMessage(Text.literal("Internal error occurred trying to bind key.").formatted(Formatting.RED));
-                return;
-            }
-
-            if (AuthorisedKeys.bind(payload.user, KeyEncode.encodePublic(payload.publicKey), true))
-                context.player().sendMessage(Text.literal("Rebound your new key to your username!").formatted(Formatting.GREEN));
-            else
-                context.player().sendMessage(Text.literal("Your new key has been bound to your username!").formatted(Formatting.GREEN));
-        });
     }
 
     public static void checkForExpiredChallenges() {
@@ -174,5 +101,92 @@ public class MCOfflineAuth implements ModInitializer {
         LOGGER.warn("extensive testing. I am not responsible for any griefed servers.");
         LOGGER.warn("It is always better to avoid offline mode if possible.");
         LOGGER.warn("USE THIS SOFTWARE AT YOUR OWN RISK.");
+    }
+
+    static class ChallengeState {
+        static final int CHALLENGE_TIMEOUT = 5;
+
+        private final Instant expiration;
+        public final byte[] data;
+
+        public ChallengeState(byte[] data) {
+            expiration = Instant.now().plusSeconds(CHALLENGE_TIMEOUT);
+            this.data = data;
+        }
+
+        public boolean isExpired() {
+            return Instant.now().isAfter(expiration);
+        }
+    }
+
+    static class LoginTask implements ConfigTaskExecutor {
+
+        @Override
+        public boolean runTask(ServerConfigContext context) {
+            if (!context.canSend(LoginChallengePayload.ID)) {
+                context.handler().onDisconnected(new DisconnectionInfo(Text.of("Client does not have MCOfflineAuth installed.")));
+                context.handler().disconnect(Text.of("Access denied. D:"));
+                return false;
+            }
+
+            LoginChallengePayload payload = spawnChallenge();
+            context.send(payload);
+            return true;
+        }
+    }
+
+    static class LoginResponseReceiver implements PacketReceiver<lol.bai.badpackets.api.config.ServerConfigContext, balls.jl.mcofflineauth.net.LoginResponsePayload> {
+
+        @Override
+        public void receive(ServerConfigContext context, LoginResponsePayload payload) {
+            checkForExpiredChallenges();
+            context.finishTask(Constants.LOGIN_TASK);
+
+            ChallengeState state = CHALLENGES.remove(payload.id);
+            if (state == null) {
+                context.handler().disconnect(Text.of("Signature verification failed due to timeout. Please try again."));
+                return;
+            }
+
+            if (!AuthorisedKeys.KEYS.containsKey(payload.user)) {
+                LOGGER.warn("Connecting user {} is not in the database.", payload.user);
+
+                // Skip verification for unbound usernames if it's allowed.
+                if (ServerConfig.allowsUnboundUsers())
+                    return;
+
+                // Else, kick them.
+                context.handler().disconnect(Text.of("You cannot join without being bound in advance. Contact a server admin to let you in."));
+                return;
+            }
+
+            if (!AuthorisedKeys.verifySignature(payload.user, state.data, payload.signature)) {
+                context.handler().disconnect(Text.of("Unable to verify your identity (incorrect signature); do you have the correct key?"));
+                return;
+            }
+
+            LOGGER.info("Verified {}'s identity successfully.", payload.user);
+        }
+    }
+
+    static class PubkeyBindReceiver implements PacketReceiver<lol.bai.badpackets.api.play.ServerPlayContext, balls.jl.mcofflineauth.net.PubkeyBindPayload> {
+
+        @Override
+        public void receive(ServerPlayContext context, PubkeyBindPayload payload) {
+            context.server().execute(() -> {
+                checkForExpiredChallenges();
+
+                if (!Objects.equals(payload.user, context.player().getName().getString())) {
+                    LOGGER.warn("Public-key payload username \"{}\" does not match the user who sent it!", payload.user);
+                    context.player().sendMessage(Text.literal("Internal error occurred trying to bind key.").formatted(Formatting.RED));
+                    return;
+                }
+
+                if (AuthorisedKeys.bind(payload.user, KeyEncode.encodePublic(payload.publicKey), true))
+                    context.player().sendMessage(Text.literal("Rebound your new key to your username!").formatted(Formatting.GREEN));
+                else
+                    context.player().sendMessage(Text.literal("Your new key has been bound to your username!").formatted(Formatting.GREEN));
+            });
+        }
     }
 }
