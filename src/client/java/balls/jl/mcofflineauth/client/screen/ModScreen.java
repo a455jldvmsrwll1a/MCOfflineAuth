1ddb0c748ffddf639dd844adcfaf287afebb7df2
Jann Atienza <minelolpjl@gmail.com>
2025-02-21 17:41:10 -0600

    Clean up mod configuration screen for client.

--- src/client/java/balls/jl/mcofflineauth/client/screen/ModScreen.java
+++ src/client/java/balls/jl/mcofflineauth/client/screen/ModScreen.java
@@ -44,12 +44,14 @@ public class ModScreen extends Screen {
 
         ButtonWidget explorerBtnWidget = ButtonWidget.builder(Text.of("Open Folder"), (btn) -> net.minecraft.util.Util.getOperatingSystem().open(Constants.MOD_DIR)).tooltip(Tooltip.of(Text.literal("Open the mod's directory with the native file manager."))).dimensions(15 + 80 + 80 + 10 + 10, height - this.textRenderer.fontHeight - 20, 100, 20).build();
         ButtonWidget cancelBtnWidget = ButtonWidget.builder(Text.of("Back"), (btn) -> close()).dimensions(width - 80 - 15, height - this.textRenderer.fontHeight - 20, 80, 20).build();
+        ButtonWidget modPageWidget = ButtonWidget.builder(Text.of("Mod Page"), (btn) -> net.minecraft.util.Util.getOperatingSystem().open("https://github.com/a455jldvmsrwll1a/MCOfflineAuth?tab=readme-ov-file#usage-players")).tooltip(Tooltip.of(Text.literal("Visit mod page on Github."))).dimensions(15, 130 - this.textRenderer.fontHeight - 20, 80, 20).build();
 
+        this.addDrawableChild(cancelBtnWidget);
+        this.addDrawableChild(modPageWidget);
         this.addDrawableChild(pkBtnWidget);
         this.addDrawableChild(resetBtnWidget);
         this.addDrawableChild(reloadBtnWidget);
         this.addDrawableChild(explorerBtnWidget);
-        this.addDrawableChild(cancelBtnWidget);
     }
 
     @Override
@@ -57,13 +59,10 @@ public class ModScreen extends Screen {
         super.render(context, mouseX, mouseY, delta);
 
         context.drawText(this.textRenderer, "MC Offline Auth Configuration", 15, 40 - this.textRenderer.fontHeight - 10, 0xFFA719FF, true);
-        context.drawText(this.textRenderer, "By JL :>", 15, 60 - this.textRenderer.fontHeight - 10, 0xFF85F1FF, true);
+        context.drawText(this.textRenderer, "By jldmw1a.", 15, 60 - this.textRenderer.fontHeight - 10, 0xFF85F1FF, true);
         context.drawText(this.textRenderer, "Allows servers to have some form of authentication without using", 15, 80 - this.textRenderer.fontHeight - 10, 0xFFFFFFFF, true);
         context.drawText(this.textRenderer, "external authentication systems, lending itself to offline servers.", 15, 90 - this.textRenderer.fontHeight - 10, 0xFFFFFFFF, true);
-        context.drawText(this.textRenderer, "DISCLAIMER: THIS MOD IS EXPERIMENTAL SOFTWARE, IT HAS NOT UNDERGONE", 15, 105 - this.textRenderer.fontHeight - 10, 0xFFEB9B34, true);
-        context.drawText(this.textRenderer, "EXTENSIVE TESTING. I AM NOT RESPONSIBLE FOR ANY GRIEFED SERVERS.", 15, 115 - this.textRenderer.fontHeight - 10, 0xFFEB9B34, true);
-        context.drawText(this.textRenderer, "IT IS ALWAYS BETTER TO AVOID OFFLINE MODE IF POSSIBLE.", 15, 125 - this.textRenderer.fontHeight - 10, 0xFFEB9B34, true);
-        context.drawText(this.textRenderer, "USE THIS MOD AT YOUR OWN RISK.", 15, 135 - this.textRenderer.fontHeight - 10, 0xFFEB9B34, true);
+        context.drawText(this.textRenderer, "THIS MOD IS EXPERIMENTAL SOFTWARE. See webpage below for usage guide.", 15, 105 - this.textRenderer.fontHeight - 10, 0xFFEB9B34, true);
         context.drawText(this.textRenderer, "Public key: ", 15, 150 - this.textRenderer.fontHeight - 10, 0xFFFFFFFF, true);
     }
 
