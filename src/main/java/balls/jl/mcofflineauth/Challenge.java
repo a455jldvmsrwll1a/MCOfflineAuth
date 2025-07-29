204009c6f7dea6cebd037409749d644f4bc1c9d3
a455jldvmsrwll1a <51287554+a455jldvmsrwll1a@users.noreply.github.com>
2025-07-29 18:05:44 -0500

    Keep track of the socket address the challenge was issued for.

--- src/main/java/balls/jl/mcofflineauth/Challenge.java
+++ src/main/java/balls/jl/mcofflineauth/Challenge.java
@@ -1,16 +1,19 @@
 package balls.jl.mcofflineauth;
 
+import java.net.SocketAddress;
 import java.time.Instant;
 
 public class Challenge {
     static final int CHALLENGE_TIMEOUT = 5000;
     public final String user;
+    public final SocketAddress address;
     public final byte[] data;
     private final Instant expiration;
 
-    public Challenge(String user, byte[] data) {
+    public Challenge(String user, SocketAddress address, byte[] data) {
         expiration = Instant.now().plusMillis(CHALLENGE_TIMEOUT);
         this.user = user;
+        this.address = address;
         this.data = data;
     }
 
