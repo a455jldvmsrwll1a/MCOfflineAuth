5cc4f4275036714a9e19cd7a87e69aef091f81a9
a455jldvmsrwll1a <51287554+a455jldvmsrwll1a@users.noreply.github.com>
2025-07-29 18:20:51 -0500

    Convert Challenge into a record.

--- src/main/java/balls/jl/mcofflineauth/Challenge.java
+++ src/main/java/balls/jl/mcofflineauth/Challenge.java
@@ -3,20 +3,7 @@ package balls.jl.mcofflineauth;
 import java.net.SocketAddress;
 import java.time.Instant;
 
-public class Challenge {
-    static final int CHALLENGE_TIMEOUT = 5000;
-    public final String user;
-    public final SocketAddress address;
-    public final byte[] data;
-    private final Instant expiration;
-
-    public Challenge(String user, SocketAddress address, byte[] data) {
-        expiration = Instant.now().plusMillis(CHALLENGE_TIMEOUT);
-        this.user = user;
-        this.address = address;
-        this.data = data;
-    }
-
+public record Challenge(String user, SocketAddress address, byte[] data, Instant expiration) {
     public boolean isExpired() {
         return Instant.now().isAfter(expiration);
     }
