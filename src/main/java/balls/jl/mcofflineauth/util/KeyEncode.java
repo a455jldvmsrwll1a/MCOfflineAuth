12e2584e33cceb190353fadf10cb3669da32584a
a455jldvmsrwll1a <51287554+a455jldvmsrwll1a@users.noreply.github.com>
2025-12-28 21:49:12 -0600

    Allow shortened versions of the encoded key string.

--- src/main/java/balls/jl/mcofflineauth/util/KeyEncode.java
+++ src/main/java/balls/jl/mcofflineauth/util/KeyEncode.java
@@ -8,13 +8,28 @@ import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
 
 public class KeyEncode {
+    // ASN.1 DER header in base64.
+    private final static String PREFIX = "MCowBQYDK2VwAyEA";
+    private final static int PREFIX_LENGTH = PREFIX.length();
+    private final static int FULL_LENGTH = 59;
+    private final static int SHORT_LENGTH = FULL_LENGTH - PREFIX_LENGTH;
+
     /// Encode public key as URL-safe base 64 but without trailing '='.
     public static String encodePublic(PublicKey key) {
-        return BytesEncode.encode(key.getEncoded());
+        return BytesEncode.encode(key.getEncoded()).substring(PREFIX_LENGTH);
     }
 
     /// Decodes a base 64 string as a public key.
     public static PublicKey decodePublic(String encoded) throws IllegalArgumentException {
+        if (encoded.length() != FULL_LENGTH) {
+            if (encoded.length() != SHORT_LENGTH) {
+                throw new IllegalArgumentException("Not a valid full or shortened public key string.");
+            }
+
+            // Make into full string.
+            encoded = PREFIX + encoded;
+        }
+
         try {
             byte[] keyBytes = BytesEncode.decode(encoded);
             KeyFactory kf = KeyFactory.getInstance(Constants.ALGORITHM);
